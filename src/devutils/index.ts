bbd6de78a142fd13389957d507610df54a4859dd
Vilperi <p-makela@outlook.com>
2022-09-08 22:21:41 +0300

    Remove tweakpane variable

--- src/devutils/index.ts
+++ src/devutils/index.ts
@@ -2,5 +2,5 @@ import { DevTool } from "@excaliburjs/dev-tools";
 import { AllProps } from "../types";
 
 export const useDevUtils = (props: AllProps) => {
-	const tweakPane = new DevTool(props.game);
+	new DevTool(props.game);
 };
